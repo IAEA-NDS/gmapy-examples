b5197c2b3f9168d08ffda1c66a01f94131048580
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-07-03 14:29:20 +0200

    remove `conda` dependency from `setup_and_run_gmapy_pipeline.sh`

--- examples/tensorflow/aws_pipeline/setup_and_run_gmapy_pipeline.sh
+++ examples/tensorflow/aws_pipeline/setup_and_run_gmapy_pipeline.sh
@@ -11,7 +11,7 @@ fi
 # inline script that will be saved to a file
 # and called. Checks if results are available
 # and then copy them to s3 storage and terminate instance.
-watchguard_script=$(cat <<'EOF'
+export watchguard_script=$(cat <<'EOF'
 #!/bin/bash
 example_name="$1"
 while [ ! -e "finished_sampling" ]; do
@@ -47,28 +47,27 @@ sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" \
 sudo apt update
 sudo apt install -y build-essential
 sudo apt install -y awscli
-
-# install the miniconda Python package manager
-cd /home/ubuntu
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-INSTALL_PATH=/home/ubuntu/miniconda
-MINICONDA_SCRIPT=Miniconda3-latest-Linux-x86_64.sh
-bash "$MINICONDA_SCRIPT" -b -p $INSTALL_PATH
-source "$INSTALL_PATH/etc/profile.d/conda.sh"
-conda init bash
-source /home/ubuntu/.bashrc
+sudo apt install -y libsuitesparse-dev
+sudo apt install -y python-is-python3
+sudo apt install -y python3-pip
+sudo apt install -y python3.10-venv
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
 
 # prepare the gmapy package (including the examples)
-git clone -b endfb81_pu9_covmat https://github.com/iaea-nds/gmapy
-conda env create -f gmapy/environment.yml -n gmapy-conda
-cd gmapy/examples/tensorflow/$example_name
+git clone --recurse-submodules -b dabbling https://github.com/iaea-nds/gmapy-examples
+cd gmapy-examples
+python -m venv calcvenv
+source calcvenv/bin/activate
+python -m pip install ./gmapy
+deactivate
 
-echo -e "$watchguard_script" > watchguard_script.sh
+echo -e "$watchguard_script" > "examples/tensorflow/$example_name/watchguard_script.sh"
 
 # run calculation pipeline
 echo "---- Starting the gmapy calculation pipeline ----"
 screen -S gmapy_session -d -m
-screen -S gmapy_session -X stuff "conda activate gmapy-conda &&"
+screen -S gmapy_session -X stuff "source calcvenv/bin/activate &&"
+screen -S gmapy_session -X stuff "cd \"examples/tensorflow/$example_name\" &&"
 screen -S gmapy_session -X stuff "python 01_model_preparation.py && touch finished_preparation &&"
 screen -S gmapy_session -X stuff "python 02_parameter_optimization.py && touch finished_optimization &&"
 screen -S gmapy_session -X stuff "python 03_mcmc_sampling.py && touch finished_sampling &&"
